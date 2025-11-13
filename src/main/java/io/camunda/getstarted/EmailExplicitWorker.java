31203e4884ca3c3cb5436b480b8a2fcb366b75b2
Pierre-Yves <pierre-yves.monnet@laposte.net>
2025-11-13 14:43:58 -0800

    Hass variables in the handle method

--- src/main/java/io/camunda/getstarted/EmailExplicitWorker.java
+++ src/main/java/io/camunda/getstarted/EmailExplicitWorker.java
@@ -6,6 +6,9 @@ import io.camunda.zeebe.client.api.worker.JobHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EmailExplicitWorker implements JobHandler {
   private final static Logger logger = LoggerFactory.getLogger(EmailAnnotationWorker.class);
 
@@ -13,6 +16,8 @@ public class EmailExplicitWorker implements JobHandler {
   public void handle(JobClient client, ActivatedJob job) throws Exception {
     final String message_content = (String) job.getVariablesAsMap().get("message_content");
     logger.info(">>>>>>>>>>>>>>> Sending explicit-email with message content: {}", message_content);
-    client.newCompleteCommand(job).send();
+    Map<String,Object> variables = new HashMap<>();
+    variables.put("message_content", message_content);
+    client.newCompleteCommand(job).variables(variables).send();
   }
 }
